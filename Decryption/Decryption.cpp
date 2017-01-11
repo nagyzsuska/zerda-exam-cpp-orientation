9a9160af82a27c7125914a83c5fe20c4fc65627e
Nagy Zsuzsanna <nagyzsuska@live.com>
2017-01-11 13:13:05 +0100

    Decryption

--- Decryption/Decryption.cpp
+++ Decryption/Decryption.cpp
@@ -6,12 +6,12 @@ Decryption::Decryption() {
 
 char Decryption::do_decryption(char c, int size) {
   char decrypted_char = ' ';
-  decrypted_char = c + size; // ez még nem kezeli, hogy jól ugorjon
-  /*if (c + size > 122) {
-
+ // decrypted_char = c + size; // ez még nem kezeli, hogy jól ugorjon
+  if (c + size > 122 || c + size < 97 && c + size > 91 ) {
+    decrypted_char = c + size - 26;
   }
   else {
-    
-  }*/
+    decrypted_char = c + size;
+  }
   return decrypted_char;
 }
